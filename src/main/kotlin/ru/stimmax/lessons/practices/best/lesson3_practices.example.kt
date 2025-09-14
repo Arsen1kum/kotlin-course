48683767475613ae5f5d3566d184d6dac2f2617b
Arsen1kum <135107758+Arsen1kum@users.noreply.github.com>
2025-09-14 12:57:40 +0200

    New Package Creation, Best naming practices notes

---
+++ src/main/kotlin/ru/stimmax/lessons/practices/best/lesson3_practices.example.kt
@@ -0,0 +1,10 @@
+package Kotlin.example.ru.stimmax.lessons.practices.best
+
+//Как называть Artifact ID, Group ID
+//        Примеры сокращения имени/названия компании и названия проекта
+//        Компания: International Cybernatic Technologies Corporation
+//        Group.ID: con.icybertech
+//        Artifact.ID: acdas
+//        Package: con.icybertech.acdas
+
+//Если название составное желательно использовать не нижнее подчёркивание "_"
